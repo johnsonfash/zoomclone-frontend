d0f6e59a9b695bb3483ff9056ed907b6fe66b11f
johnsonfash <tosin.fashanu@supermartng.com>
2022-03-22 16:35:13 +0100

    changes

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -5,7 +5,8 @@ function Video({ id, stream }) {
   const ref = useRef();
   useEffect(() => {
     setTimeout(() => {
-      ref.current.srcObject = stream;
+      ref.current.src = window.URL.createObjectURL(stream);
+      // ref.current.srcObject = stream;
     }, 500);
   }, [stream]);
 
