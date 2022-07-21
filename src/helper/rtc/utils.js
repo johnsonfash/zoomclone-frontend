0b9ff3cc458a989126af2e29a055a4ec5d5e9aa1
johnsonfash <fashanutosin7@gmail.com>
2022-07-21 07:31:00 +0100

    change video facing mode error

--- src/helper/rtc/utils.js
+++ src/helper/rtc/utils.js
@@ -1,14 +1,16 @@
 export const mediaStream = async (callback, audio = true, video = true) => {
   window.navigator.mediaDevices.getUserMedia({
-    audio: {
-      echoCancellation: true,
-      noiseSuppression: true
-    },
-     video: {
-      facingMode: {
-        exact: 'user'
-      }
-    }
+    // audio: {
+    //   echoCancellation: true,
+    //   noiseSuppression: true
+    // },
+    //  video: {
+    //   facingMode: {
+    //     exact: 'user'
+    //   }
+    // }
+    audio: true,
+    video: true
   }).then((stream) => {
     callback(stream)
   })
